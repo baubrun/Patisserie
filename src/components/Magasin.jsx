eb86baf40ba71d2ad625a31673459764c264e513
baubrun <baubelf@gmail.com>
2020-06-21 17:24:30 -0400

    fixed msg

--- src/components/Magasin.jsx
+++ src/components/Magasin.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Button from "./Button";
 import FormInput from "./FormInput";
 import { Link } from "react-router-dom";
 import ItemCard from "./ItemCard";
